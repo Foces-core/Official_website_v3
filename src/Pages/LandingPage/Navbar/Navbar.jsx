9500d736b6c8892247733b779b28bbf345cc95b1
SHIJIN ABRAHAM <shijinabraham2003@gmail.com>
2024-01-17 20:04:53 +0530

    New updates

--- src/Pages/LandingPage/Navbar/Navbar.jsx
+++ src/Pages/LandingPage/Navbar/Navbar.jsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from "react";
 import "./Navbar.css";
 
-export default function Navbar() {
-  const [showItems, setShowItems] = useState(false);
+export default function Navbar({showItems, setShowItems}) {
   const [isMobile, setIsMobile] = useState(false);
 
   const toggleItems = () => {
@@ -32,7 +31,7 @@ export default function Navbar() {
     <div className={`shadow nav bg-transparent flex min-[767px]:items-center p-10 font-bold max-[767px]:pl-4  max-[767px]:pt-8 cursor-none ${isMobile && showItems ? "": "bg-black"}`}>
       {isMobile && (
         <div
-          className="h-[2rem] w-[2rem] Button absolute inset-y-8 right-5 flex items-center top-[4.1%] cursor-none"
+          className="h-[2rem] w-[2rem] Button absolute inset-y-10 right-5 flex items-center top-[31%] cursor-none"
           onClick={toggleItems}
         ></div>
       )}
@@ -53,7 +52,7 @@ export default function Navbar() {
       )}
 
       <div
-        className={`container z-0 bg-transparent Items flex justify-center space-x-9 text-[white] max-[767px]:z-10 max-[767px]:bg-black max-[767px]:flex-col max-[767px]:w-screen max-[767px]:-ml-4 max-[767px]:items-center max-[767px]:space-x-0 max-[767px]:absolute max-[767px]:my-10 max-[767px]:gap-7 max-[767px]:pb-10 ${
+        className={`container z-10 bg-transparent Items flex justify-center space-x-9 text-[white] max-[767px]:bg-[#101011] max-[767px]:flex-col max-[767px]:w-screen max-[767px]:-ml-4 max-[767px]:items-center max-[767px]:space-x-0 max-[767px]:absolute max-[767px]:h-[70vh] max-[767px]:mt-10 max-[767px]:gap-7 max-[767px]:pb-10 ${
           showItems ? "" : "hidden"
         } ${isMobile && showItems ? "h-[80%]" : ""}`}
       >
