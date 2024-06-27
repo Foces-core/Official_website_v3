ef2bc8fb8129ac9945d5c94a562abf1524f322ea
Jerry Sanju Joanes <jerrysanjujoanes000@gmail.com>
2024-06-27 21:06:58 +0530

    sloving deployment error

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   assetsInclude: ['**/*.glb'],
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-})
+  optimizeDeps: {
+    include: ['@sanity/client'],
+  },
+});
