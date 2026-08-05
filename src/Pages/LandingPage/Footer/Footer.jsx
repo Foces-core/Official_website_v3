44607213df9df8e2cb0abf0c6426eb62faee3db1
Sebin Mathew <sebinmathew543@gmail.com>
2026-08-05 17:01:12 +0530

    fix(footer): import FocesLogo SVG and CollegeLogo PNG directly to resolve broken question mark 404 image box

--- src/Pages/LandingPage/Footer/Footer.jsx
+++ src/Pages/LandingPage/Footer/Footer.jsx
@@ -1,41 +1,41 @@
-import React, { useEffect } from "react";
-import { FaSquareFacebook } from "react-icons/fa6";
-import { FaInstagram } from "react-icons/fa6";
-import { FaXTwitter } from "react-icons/fa6";
-import { FaLinkedin } from "react-icons/fa6";
-import { FaHeart } from "react-icons/fa6";
+import React from "react";
+import { FaSquareFacebook, FaInstagram, FaXTwitter, FaLinkedin, FaHeart } from "react-icons/fa6";
+import FocesLogo from "../../../assets/FOCES White.svg";
+import CollegeLogo from "../../../assets/clogo.png";
 import "./Footer.css";
 
 function Footer() {
   return (
-    <div className="h-[10vh] bg-[#101011] relative z-0 flex w-screen items-center gap-10 p-5 max-[767px]:flex-col mt-20 max-[767px]:mt-5">
-      <div className="w-[15%] max-[767px]:w-[50%]">
-        <img src="././src/assets/FOCES.png" alt="" />
+    <footer className="h-auto md:h-[12vh] bg-[#101011] relative z-0 flex flex-col md:flex-row w-full items-center justify-between gap-6 p-6 mt-12 border-t border-white/10">
+      <div className="w-full md:w-[20%] flex justify-center md:justify-start items-center">
+        <img src={FocesLogo} alt="FOCES Logo" className="h-8 w-auto opacity-90 hover:opacity-100 transition-opacity" />
       </div>
-      <div className="flex flex-col justify-between items-center w-[70%] gap-10">
-        <div className="social flex gap-16 max-[767px]:w-screen max-[767px]:justify-center">
-          <a href="https://www.facebook.com/focescec?mibextid=JRoKGi" target="_blank">
-            <FaSquareFacebook className="h-5 w-5 text-[#D9D9D9] hover:scale-125 duration-300" />
+      
+      <div className="flex flex-col justify-between items-center w-full md:w-[60%] gap-4">
+        <div className="social flex gap-8 justify-center items-center">
+          <a href="https://www.facebook.com/focescec?mibextid=JRoKGi" target="_blank" rel="noreferrer" aria-label="Facebook">
+            <FaSquareFacebook className="h-5 w-5 text-[#D9D9D9] hover:text-white hover:scale-125 duration-300" />
           </a>
-          <a href="https://www.instagram.com/foces_cec?igsh=b2E3bjNpbGgzdG03" target="_blank">
-            <FaInstagram className="h-5 w-5 text-[#D9D9D9] hover:scale-125 duration-300" />
+          <a href="https://www.instagram.com/foces_cec?igsh=b2E3bjNpbGgzdG03" target="_blank" rel="noreferrer" aria-label="Instagram">
+            <FaInstagram className="h-5 w-5 text-[#D9D9D9] hover:text-white hover:scale-125 duration-300" />
           </a>
-          <a href="https://x.com/foces_cec?t=e__UXOl9tQFznh7JG8kqzQ&s=08" target="_blank">
-            <FaXTwitter className="h-5 w-5 text-[#D9D9D9] hover:scale-125 duration-300" />
+          <a href="https://x.com/foces_cec?t=e__UXOl9tQFznh7JG8kqzQ&s=08" target="_blank" rel="noreferrer" aria-label="Twitter">
+            <FaXTwitter className="h-5 w-5 text-[#D9D9D9] hover:text-white hover:scale-125 duration-300" />
           </a>
-          <a href="https://www.linkedin.com/in/foces-cec-423176229/" target="_blank">
-            <FaLinkedin className="h-5 w-5 text-[#D9D9D9] hover:scale-125 duration-300" />
+          <a href="https://www.linkedin.com/in/foces-cec-423176229/" target="_blank" rel="noreferrer" aria-label="LinkedIn">
+            <FaLinkedin className="h-5 w-5 text-[#D9D9D9] hover:text-white hover:scale-125 duration-300" />
           </a>
         </div>
-        <p className="text-center text-[#D9D9D9] max-[767px]:w-screen max-[767px]:text-sm max-[380px]:text-xs">
-          Copyright ©2023 All rights reserved | Made with{" "}
-          <FaHeart className="inline-block text-[#080AA4] h-4 w-4" /> by FOCES
+        <p className="text-center text-[#D9D9D9] text-xs md:text-sm">
+          Copyright ©2026 All rights reserved | Made with{" "}
+          <FaHeart className="inline-block text-[#080AA4] h-3.5 w-3.5 mx-0.5" /> by FOCES
         </p>
       </div>
-      <div className="w-[15%] max-[767px]:hidden">
-        <img src="././src/assets/clogo.png" alt="" className="scale-[80%]" />
+
+      <div className="w-full md:w-[20%] hidden md:flex justify-end items-center">
+        <img src={CollegeLogo} alt="College Logo" className="h-10 w-auto opacity-70 hover:opacity-100 transition-opacity" />
       </div>
-    </div>
+    </footer>
   );
 }
 
