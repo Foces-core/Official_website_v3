2f60b9579f82124e458a2842fba1090ff1075aa8
Sebin Mathew <sebinmathew543@gmail.com>
2026-08-05 15:37:17 +0530

    fix(ui): remove floating ToTopButton and white side notification toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Footer from "./Pages/LandingPage/Footer/Footer";
 import AboutUs from "./Components/AboutUs/AboutUs";
 import Execom from "./Components/Execom/Execom";
 import Navbar from "./Pages/LandingPage/Navbar/Navbar";
-import ToTopButton from "./Components/ToTopButton";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useLocation } from "react-router-dom";
@@ -30,7 +29,6 @@ function App() {
 
   return (
     <div className="App bg-[#101011]">
-      <ToTopButton />
       <Navbar />
       <HeroSection />
       <AboutUs />
--- src/Pages/LandingPage/HeroSection/HeroSection.jsx
+++ src/Pages/LandingPage/HeroSection/HeroSection.jsx
@@ -85,38 +85,11 @@ function HeroSection() {
 
   return (
     <div className='HeroSection relative bg-transparent overflow-hidden h-screen cursor-none' id='home' ref={myRef}>
-      <div className={`hero ${showNotifications ? 'blur-sm' : ''}`}>
+      <div className="hero">
         <Cursor />
         <img src={ddd} alt="DDD" className={`h-[50%] w-[36%] relative top-[40vh] left-[10vw] max-[767px]:w-[80%] max-[767px]:top-[38vh] `} />
         <img src={focespng} alt="FOCES" className={`h-[50%] w-[38%] relative top-[45vh] left-[10vw] max-[767px]:w-[80%] max-[767px]:top-[40vh] `} />
         <img src={foces1} alt="" className={`h-[50%] w-[38%] relative top-[50vh] left-[10vw] max-[767px]:w-[80%] max-[767px]:top-[41vh] `} />
-        <img src={Notification} alt="Notif" className={`Notif h-[26vh] absolute translate-y-80 right-0 cursor-pointer max-[767px]:mt-[35%] max-[767px]:z-0 ${showNotifications ? 'hidden' : ''}`} onTouchStart={handleImageClick} onClick={handleImageClick} />
-      </div>
-      <div ref={notificationsRef} className={`notifications h-[60%] w-[28%] absolute bottom-3 max-[767px]:bottom-5 right-0 bg-opacity-45 bg-slate-900 rounded-3xl overflow-scroll overflow-x-hidden max-[767px]:w-[90%] max-[767px]:z-10 ${showNotifications ? 'visible' : 'translate-x-[110%]'} ${isMobile ? 'h-[50vh] bottom-[7%] right-[5%]' : 'right-3'}`}>
-        {notfy.map((event) => (
-          <div key={event.id} className={`text-[#D9D9D9] flex h-fit flex-col my-10 p-9 w-[100%] pr-4 ${event.id === 1 ? 'mt-0' : ''}`}>
-            <div className="notif flex justify-between">
-              <div className=''>
-                <img src={speaker} alt="" className='border border-[#2a5739] rounded-md' />
-                <div key={event.id} className={`line w-[2px] h-[320%] ml-[30%] bg-[#C2C2C2] ${event.id === 4 ? 'h-28' : ''}`}></div>
-              </div>
-              <div className=''>
-                <img src={clock} alt="" />
-                <p className='text-sm'>{event.date}</p>
-              </div>
-            </div>
-            <div className='w-[85%] absolute h-fit mt-[12%] ml-[13%] content'>
-              <h3 className='font-semibold'>{event.Event_name}</h3>
-              <p className='text-sm max-[767px]:w-[95%]'>
-                <BlockContent
-                  blocks={event.short_details}
-                  projectId='n7hx0w67'
-                  dataset="production"
-                />
-              </p>
-            </div>
-          </div>
-        ))}
       </div>
     </div>
   );
